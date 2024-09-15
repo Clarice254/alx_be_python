0537ff748cb17570facff55ce946c1f0ac42c82c
Clarice Kavele <kaveleclarice@gmail.com>
2024-09-15 16:21:05 +0300

    Add simple interest script

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,8 @@
+#Define Variables
+principal = 1000 #in $
+rate = 0.05      #5% interest rate
+time = 3         #Years
+#Simple interest calculation
+interest = principal * rate * time
+#The interest results
+print(f"the simple interest is {interest}")  
