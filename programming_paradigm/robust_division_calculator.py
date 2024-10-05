dff25fe8b0f92d259984bdc0d93f9d13c1c612e6
Clarice Kavele <kaveleclarice@gmail.com>
2024-10-05 06:10:26 +0300

    Add changes to division calculator that robustly handles errors like division by zero and non-numeric inputs.

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -7,11 +7,10 @@ def safe_divide(numerator, denominator):
         result = num1 / num2
 
     except ZeroDivisionError:
-        print("Error: Cannot divide by zero.")
+        return "Error: Cannot divide by zero."
 
     except ValueError:
-        print("Error: Please enter numeric values only.")
+        return "Error: Please enter numeric values only."
 
-    else:
-        print(f"The result of the division is {result}")
+    return f"The result of the division is {result}"
     
